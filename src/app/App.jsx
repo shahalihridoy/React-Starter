c8972171b251302e51ea82230d67bc4ff7f9cab6
Shah Ali Hriody <shahalihridoy@gmail.com>
2019-07-16 07:13:26 -0700

    first commit

---
+++ src/app/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/app.scss';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1>sdkfjsjdfksdfslkdjfsa</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
